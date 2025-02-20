a775d6ba1a35a2e3447a6079a68bb5fffe26dba1
Leidyph2002 <leidypasacaherrera@gmail.com>
2025-02-20 18:41:19 +0100

    Initial commit

---
+++ php_mysql_crud/config/database.php
@@ -0,0 +1,15 @@
+<?php
+// Definir constantes para la configuración de la base de datos
+define('DB_HOST', 'localhost'); // Servidor de la base de datos
+define('DB_USER', 'root'); // Usuario de la base de datos
+define('DB_PASS', 'Melones2023.'); // Contraseña de la base de datos
+define('DB_NAME', 'crud_db'); // Nombre de la base de datos
+
+// Crear conexión a la base de datos utilizando MySQLi
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Verificar si la conexión fue exitosa
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error); // Mostrar mensaje de error y detener la ejecución
+}
+?>
